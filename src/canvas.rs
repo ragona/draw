d1d89fc9740676fea749c5e1664727b6e563f9d4
ragona <ryan@ragona.com>
2019-08-29 08:37:01 -0700

    scaffolding

--- src/canvas.rs
+++ src/canvas.rs
@@ -5,6 +5,9 @@ use svg::node::element::Element;
 use svg::node::NodeClone;
 use svg::{Document, Node};
 
+/// Children are stored in a vector; this `usize` is a handle to access the child
+pub type DrawingId = usize;
+
 pub struct Canvas {
     pub width: u32,
     pub height: u32,
@@ -20,5 +23,11 @@ impl Canvas {
         }
     }
 
-    pub fn add(&mut self) {}
+    /// Adds a drawing to the top of the canvas display list.
+    /// Returns a DrawingId handle that can be used to refer to the drawing in the future.
+    pub fn add(&mut self, drawing: Drawing) -> DrawingId {
+        let child_id = self.drawings.len();
+        self.drawings.push(drawing);
+        child_id
+    }
 }
