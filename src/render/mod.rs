9b1c450fa824bdaa876d6649f5a104cf5cbfbde3
ragona <ryanragona@gmail.com>
2019-09-01 16:25:30 -0700

    create folder if it doesn't exist

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,5 +1,5 @@
 //! Renders Canvas objects to bytes, allows for saving to disk.
-use std::fs::File;
+use std::fs::{self, File};
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
@@ -22,7 +22,20 @@ pub trait Renderer {
 /// * `renderer` - An object that implements the `Renderer` trait
 pub fn save(canvas: &Canvas, path: &str, renderer: impl Renderer) -> io::Result<()> {
     let path = Path::new(path);
+    if path.is_dir() {
+        panic!("Warning: Attempted to save file over an existing directory");
+    }
+    // create folder if it doesn't exist
+    if path.parent().is_none() {
+        panic!("Failed to find parent directory")
+    }
+    let folder = path.parent().unwrap();
+    if !Path::exists(folder) {
+        fs::create_dir_all(folder)?;
+    }
+    // render the canvas
     let bytes = renderer.render(canvas);
+    // save to disk, overwrite
     let mut file = File::create(&path)?;
     file.write_all(&bytes)?;
 
