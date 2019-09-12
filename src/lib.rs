bfe2a76e0c38041be1d966ae5750c369c1dc1517
ragona <ryanragona@gmail.com>
2019-09-11 21:52:29 -0700

    making shapes optional for drawings

--- src/lib.rs
+++ src/lib.rs
@@ -59,11 +59,13 @@ use cgmath::Point2;
 use rgb;
 
 pub mod canvas;
+pub mod drawing;
 pub mod render;
 pub mod shape;
 pub mod style;
 
 pub use crate::canvas::Canvas;
+pub use crate::drawing::{DisplayList, Drawing};
 pub use crate::render::svg::SvgRenderer;
 pub use crate::shape::{LineBuilder, Shape};
 pub use crate::style::Color;
@@ -76,54 +78,3 @@ pub type Point = Point2<f32>;
 
 /// An alias for RGB<u8>
 pub type RGB = rgb::RGB<u8>;
-
-/// A drawing composes a `Shape`, a `DisplayList`, a `Position`, and a `Style`. It's a complete
-/// sub-tree of an overall piece of art.
-pub struct Drawing {
-    /// The actual background shape for the drawing
-    pub shape: Shape, // todo: Should this be an Option?
-    /// The drawing's children, layered from bottom to top
-    pub display_list: DisplayList,
-    /// The top left location for the drawing
-    pub position: Point,
-    /// Fill and stroke information for the drawing
-    pub style: Style,
-}
-
-impl Drawing {
-    /// Create a new drawing with default values.
-    pub fn new(shape: Shape) -> Drawing {
-        Drawing {
-            shape,
-            style: Style::default(),
-            display_list: DisplayList::new(),
-            position: Point::origin(),
-        }
-    }
-}
-
-/// A sorted Vec of Drawings, ordered from bottom to top.
-pub struct DisplayList {
-    drawings: Vec<Drawing>,
-}
-
-impl DisplayList {
-    /// Create a new empty display list
-    fn new() -> DisplayList {
-        DisplayList { drawings: vec![] }
-    }
-
-    /// Adds a drawing to the top of the display list.
-    /// Returns a DrawingId handle that can be used to refer to the drawing in the future.
-    pub fn add(&mut self, drawing: Drawing) -> DrawingId {
-        let child_id = self.drawings.len();
-        self.drawings.push(drawing);
-        child_id
-    }
-
-    /// Remove a drawing from the display list
-    /// todo: Implement
-    pub fn remove(&mut self, _drawing_id: DrawingId) {
-        unimplemented!()
-    }
-}
